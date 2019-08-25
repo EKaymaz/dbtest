6a08e755d24ec56533bc77c24f21d41a3b6fd3ac
ekaymaz <erdemkaymaz@gmail.com>
2019-08-25 23:20:22 +0200

    manager entity added

---
+++ src/main/java/com/dbtest/model/Manager.java
@@ -0,0 +1,54 @@
+package com.dbtest.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Manager {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "fistName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "dept")
+    private String dept;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+}
